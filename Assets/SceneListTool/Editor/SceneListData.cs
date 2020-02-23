45b186f48f7f14a175e7d02b74972d88a4b41773
Jisan Haider <>
2020-02-24 01:39:32 +0600

    Documentation, Readme Added, Unity 2017,2018 Support Added, Tool Directory Organized

--- Assets/SceneListTool/Editor/SceneListData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-
-public class SceneListData : ScriptableObject {
-    public List<string> FavoriteScenes;
-    public List<SceneAsset> BuildScenes;
-    public List<bool> BuildScenes_Enabled;
-}
