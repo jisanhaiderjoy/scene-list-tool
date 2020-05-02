c5488b915c72bf340fa4a9bf522eb0a5bc78e08c
Jisan Haider Joy <jisanhaider76@gmail.com>
2020-05-02 07:22:08 +0600

    Init Welcome Message update and BugFix

--- Assets/SceneListTool/Editor/SceneListInit.cs
+++ Assets/SceneListTool/Editor/SceneListInit.cs
@@ -6,17 +6,13 @@ namespace SceneListToolLibrary
 	public static class SceneListInit
 	{
 		static SceneListInit()
-        {
+		{
 			if (!EditorPrefs.HasKey("SceneListInit"))
 			{
 				EditorPrefs.SetInt("SceneListInit", 1);
-				if (EditorUtility.DisplayDialog("Greetings from Headshot Games!", "Thank you for purchasing Scene List Tool." +
-					"\r\rIf you liked the tool, don't forget to give a review. It will help me to make it better!\r\rYou can contact me at headshotgamesstudio@gmail.com for any support.", "Show Documentation", "Got it!"))
-				{
-					var obj = AssetDatabase.LoadAssetAtPath<DefaultAsset>("Assets/SceneListTool/Documentation/Documentation.pdf");
-					EditorGUIUtility.PingObject(obj);
-				}
+				EditorUtility.DisplayDialog("Greetings from Headshot Games!", "Thank you for purchasing Scene List Tool." +
+					"\r\rIf you liked the tool, don't forget to give a review. It will help me to make it better!\r\rYou can contact me at headshotgamesstudio@gmail.com for any support.", "Got it!");
 			}
 		}
-    }
+	}
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -9,9 +9,22 @@ public class Test : MonoBehaviour {
         UnityEditor.EditorUtility.RevealInFinder(Application.temporaryCachePath);
     }
 
+
     [Button("Open Persistant Path")]
     public void PersistantPathOpen() {
         UnityEditor.EditorUtility.RevealInFinder(Application.persistentDataPath);
     }
 
+    [Button("Delete SceneListInit EditorPrefs")]
+    public void ClearEditorPrefs()
+    {
+        UnityEditor.EditorPrefs.DeleteKey("SceneListInit");
+        Debug.Log("Cleared");
+    }
+
+    [Button("Get SceneListInit EditorPrefs")]
+    public void GetEditorPrefs()
+    {
+        Debug.Log(UnityEditor.EditorPrefs.GetInt("SceneListInit"));
+    }
 }
